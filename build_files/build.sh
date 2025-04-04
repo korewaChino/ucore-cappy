f096fa0420c577f8030da919a4d0e10889031d76
Cappy Ishihara <cappy@cappuchino.xyz>
2025-04-05 00:12:14 +0700

    oops, terra install

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,7 +8,7 @@ set -ouex pipefail
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-dnf5 install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
+dnf5 install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 # this installs a package from fedora repos
 dnf5 install -y \
     nano \
